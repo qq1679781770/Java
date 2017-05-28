60af4da9558ece5759a041924234afc70ffc70b9
qq1679781770 <1679781770@qq.com>
2017-05-28 22:37:50 +0800

    辅助

---
+++ hanlp/src/main/java/com/jsxnh/hanpl/util/Myterm.java
@@ -0,0 +1,32 @@
+package com.jsxnh.hanpl.util;
+
+import com.hankcs.hanlp.corpus.tag.Nature;
+
+public class Myterm {
+
+	private String word;
+	private Nature nature;
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((word == null) ? 0 : word.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Myterm other = (Myterm) obj;
+		if (word == null) {
+			if (other.word != null)
+				return false;
+		} else if (!word.equals(other.word))
+			return false;
+		return true;
+	}
+}
